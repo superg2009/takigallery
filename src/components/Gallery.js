9cf8ff24ae463c3f6bf2bdcf2835220c01ced51b
Grant Marshall <grantrmarshall97@gmail.com>
2020-05-16 18:15:41 -0700

    remove redundant component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import Photo from './Photo';
+import Image from 'react-bootstrap/Image';
 
 function Gallery(props){
+const styles = {
+        height: "55vmin",
+        padding: "0.15em"
+    };
 const images = props.src;
     return (
-         images.map(({id, src, title, description}) => <Photo key={id} src={src} title={title} alt={description}></Photo>)
+         images.map(({id, src, title, description}) => <Image fluid='true' key={id} src={src} title={title} alt={description} style={styles}></Image>)
     );
         
 }
