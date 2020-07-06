3cb4c3266003f4d49dc936cf7b069012f4cfa179
Grant Marshall <grantrmarshall97@gmail.com>
2020-07-06 14:55:58 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer (props){
     return(
         <div> 
          &copy; 2020 Grant Marshall <br></br>
-        <a href='www.grantmarshall.me'>Website</a>
+        <a href='http://www.grantmarshall.me'>Website</a>
         <p> for the glory of Hands <Emoji label='Hand' symbol="👋" aria-hidden = {true}></Emoji></p>
         <p>Made with React, React Bootstrap, Memes and <Emoji label='Heart' symbol='❤️' aria-hidden = {true}></Emoji></p>
         </div>
