da6637cac0f9efe839edb19489ca60a7ba6d7df8
Grant Marshall <grantrmarshall97@gmail.com>
2021-10-22 15:57:51 -0700

    formatting

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import Image from 'react-bootstrap/Image';
+import React from "react";
+import Image from "react-bootstrap/Image";
 
-function Gallery(props){
-const styles = {
-        height: "55vmin",
-        padding: "0.15em"
-    };
-const images = props.src;
-    return (
-         images.map(({id, src, title, description}) => <Image fluid='true' key={id} src={src} title={title} alt={description} style={styles}></Image>)
-    );
-        
+function Gallery(props) {
+  const styles = {
+    height: "55vmin",
+    padding: "0.15em",
+  };
+  const images = props.src;
+  return images.map(({ id, src, title, description }) => (
+    <Image
+      fluid="true"
+      key={id}
+      src={src}
+      title={title}
+      alt={description}
+      style={styles}
+    ></Image>
+  ));
 }
 
 export default Gallery;
